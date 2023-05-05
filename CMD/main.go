e6d0c832cc5fe9155941d46dee8573dc0b3f108d
Muralidhar <66506079+murali1999-tech@users.noreply.github.com>
2023-05-05 12:52:15 +0530

    Update main.go

--- CMD/main.go
+++ CMD/main.go
@@ -1,1 +1,30 @@
 
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/murali1999-tech/poker-hand-sorter/internal/hand"
+	"github.com/murali1999-tech/poker-hand-sorter/pkg/utils"
+)
+
+func main() {
+	// Read the poker hand from the command line.
+	input, err := utils.ReadInput()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+
+	// Create a new hand from the input.
+	h, err := hand.NewHand(input)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating hand:", err)
+		os.Exit(1)
+	}
+
+	// Sort the hand and print the result.
+	h.Sort()
+	fmt.Println(h.String())
+}
